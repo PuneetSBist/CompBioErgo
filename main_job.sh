36e7be0dfb3d7cf97ec5fbcd2caa7b19f2d7df28
PuneetSBist <166434647+PuneetSBist@users.noreply.github.com>
2024-12-03 14:53:25 -0700

    Old files as well new TCR train data

---
+++ main_job.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+#SBATCH -p general ## Partition
+#SBATCH -q public  ## QOS
+#SBATCH -c 1       ## Number of Cores
+#SBATCH --time=5   ## 5 minutes of compute
+#SBATCH --job-name=ood-example-python
+#SBATCH --output=slurm.%j.out  ## job /dev/stdout record (%j expands -> jobid)
+#SBATCH --error=slurm.%j.err   ## job /dev/stderr record 
+#SBATCH --export=NONE          ## keep environment clean
+#SBATCH --mail-type=ALL        ## notify <asurite>@asu.edu for any job state change
+
+echo "WHERE I AM FROM: $SLURM_SUBMIT_DIR"
+echo "WHERE AM I NOW: $(pwd)"
+echo "Hello World" > output_file
+echo "Created output file with 'Hello World'"
+
+echo "Loading python 3 from anaconda module"
+module load mamba/latest
+echo "Loading scientific computing python environment, scicomp"
+source activate python3_6
+echo "Running example python script"
+export PYTHONPATH="~/AlgoCompBio/Ergo/ERGO-master:$PYTHONPATH"
+python proj_ergo.py train lstm cpu --train_data_path ~/AlgoCompBio/data/BAP/tcr_split/train.csv --test_data_path ~/AlgoCompBio/data/BAP/tcr_split/test.csv
+
